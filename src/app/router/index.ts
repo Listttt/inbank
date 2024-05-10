0fc581083c98a3aa4af218ac472dc67eb389555d
Dmitri Tanasiitsuk <dmitri.tanasiitsuk@bigwintek.com>
2024-05-10 20:43:46 +0300

    Adjusted Server Side Rendering.
    https://blog.logrocket.com/adding-ssr-existing-vue-3-app/

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,15 +1,38 @@
-import {createRouter, createWebHistory } from 'vue-router'
-import {ROUTE_NAMES} from "@/shared/config/";
+// import {createRouter, createWebHistory } from 'vue-router'
+// import {ROUTE_NAMES} from "@/shared/config/";
+//
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: ROUTE_NAMES.HOME,
+//       component: () => import('@/pages/HomePage.vue')
+//     }
+//   ]
+// })
+//
+// export default router
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: ROUTE_NAMES.HOME,
-      component: () => import('@/pages/HomePage.vue')
-    }
-  ]
-})
+import {
+  createMemoryHistory,
+  createRouter as _createRouter,
+  createWebHistory,
+} from "vue-router";
+import {ROUTE_NAMES} from "@/shared/config";
 
-export default router
+const routes = [
+  {
+    path: "/",
+    name: ROUTE_NAMES.HOME,
+    component: () => import("@/pages/HomePage.vue"),
+  },
+];
+
+export const createRouter = () =>
+    _createRouter({
+      history: import.meta.env.SSR
+          ? createMemoryHistory("/")
+          : createWebHistory("/"),
+      routes,
+    });
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import './app/assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+//
+// import App from './App.vue'
+// import router from './app/router'
+//
+// const app = createApp(App)
+//
+// app.use(createPinia())
+// app.use(router)
+//
+// app.mount('#app')
 
-import App from './App.vue'
-import router from './app/router'
+import { createPinia } from "pinia";
+import { createSSRApp } from "vue";
+import App from "./App.vue";
+import { createRouter } from "./app/router";
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+export function createApp() {
+    const app = createSSRApp(App);
+    const pinia = createPinia();
+    app.use(pinia);
+    const router = createRouter();
+    app.use(router);
+    return { app, router };
+}
