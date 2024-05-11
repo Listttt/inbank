d6350b586e73fa7e0c7010e6e11db88afdc15d8e
Dmitri Tanasiitsuk <dmitri.tanasiitsuk@bigwintek.com>
2024-05-11 15:15:41 +0300

    Logic for reflecting header moved from template to script.

--- src/widgets/header/ui/IbHeader.vue
+++ src/widgets/header/ui/IbHeader.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import {ROUTE_NAMES} from "@/shared/config";
 import {IbLogo} from "@/shared/logo";
+import {computed} from "vue";
+import {useRoute} from "vue-router";
 
-const VISIBLE_ROUTES = [ROUTE_NAMES.HOME];
+const { name } = useRoute();
+
+const VISIBLE_ROUTES: Array<ROUTE_NAMES> = [ROUTE_NAMES.HOME];
+
+const show = computed(() => {
+  return VISIBLE_ROUTES.includes(name as ROUTE_NAMES);
+})
 
 </script>
 
 <template>
-<!--  <header v-show="VISIBLE_ROUTES.includes($route.name)">-->
-  <header v-show="true">
+  <header v-show="show">
     <ib-logo/>
   </header>
 </template>
