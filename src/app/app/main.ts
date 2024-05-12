62d74e276be11a2d573b24128052239913402ce0
Dmitri Tanasiitsuk <dmitri.tanasiitsuk@bigwintek.com>
2024-05-12 13:40:48 +0300

    Added i18n https://medium.com/@kasiakadziolka/implementation-of-i18n-in-the-vue-3-composition-api-ac29bef63c24

--- src/app/app/main.ts
+++ src/app/app/main.ts
@@ -5,10 +5,24 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from '../router'
+import {createI18n} from "vue-i18n";
+// Not corresponding to FSD methodology. Translations should be located in segments where they are used.
+// TODO: create some way to keep translations in segment and grab them after together. (No time for it now)
+import en from "../translations/en.json"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// console.log()
+const i18n = createI18n({
+    locale: navigator.language,
+    fallbackLocale: "en",
+    messages: { en },
+    legacy: false
+});
+
+app.use(i18n);
 
 app.mount('#app')
