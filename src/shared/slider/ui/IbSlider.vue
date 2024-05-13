00269dfb95e527e56f6d1c9b4e0ff443ce2c6ac1
Dmitri Tanasiitsuk <dmitri.tanasiitsuk@bigwintek.com>
2024-05-13 12:25:44 +0300

    Created IbSlider component: Custom slider component. Drawed progress bar.
    Helper: https://www.youtube.com/watch?v=YE10hytBBho

---
+++ src/shared/slider/ui/IbSlider.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import type {Ref} from "vue";
+
+const slider: Ref<HTMLInputElement> = ref<HTMLInputElement>(null as unknown as HTMLInputElement);
+
+onMounted(() => {
+  function drawProgress(slider: HTMLInputElement): void {
+    const progress: number = (+slider.value - +slider.min) / (+slider.max - +slider.min) * 100;
+    slider.style.background = 'linear-gradient(to right, #21093A 0%, #21093A '+ progress + '%, #fff ' + progress + '%, #fff 100%)'
+  }
+   slider.value.oninput = function () {
+     const self: HTMLInputElement = this as HTMLInputElement;
+     drawProgress(this as HTMLInputElement)
+   };
+
+  drawProgress(slider.value);
+})
+
+</script>
+
+<template>
+  <input ref="slider" type="range" class="ib-slider">
+</template>
+
+<style scoped>
+.ib-slider {
+  -webkit-appearance: none;
+  width: 100%;
+}
+</style>
--- src/widgets/calculator/ui/IbCalculatorWidget.vue
+++ src/widgets/calculator/ui/IbCalculatorWidget.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-
+import {IbSlider} from "@/shared/slider";
 </script>
 
 <template>
   <div class="ib-container ib-content">
     <div>text</div>
     <div>calc</div>
+
+    <ib-slider class="w-3" min="100" max="7000" value="1000"/>
   </div>
 </template>
 
