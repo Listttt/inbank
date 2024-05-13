a4f8cf01a050586b0dae360fe1a29778354efcaa
Dmitri Tanasiitsuk <dmitri.tanasiitsuk@bigwintek.com>
2024-05-13 12:58:28 +0300

    Styled slider Thumb.
    Helper with image: https://stackoverflow.com/questions/62834173/input-range-slider-replace-thumb-with-image-not-working

--- src/shared/slider/ui/IbSlider.vue
+++ src/shared/slider/ui/IbSlider.vue
@@ -27,5 +27,22 @@ onMounted(() => {
 .ib-slider {
   -webkit-appearance: none;
   width: 100%;
+  height: 4px;
+}
+
+.ib-slider::-webkit-slider-thumb{
+  -webkit-appearance: none;
+  appearance: none;
+  width: 23px;
+  height: 24px;
+  border: 0;
+  background-image: url("data:image/svg+xml,%3Csvg width='22' height='12' viewBox='0 0 22 12' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M9 6L1 6' stroke='%2321093A'/%3E%3Cpath d='M6 1L1 6L6 11' stroke='%2321093A'/%3E%3Cpath d='M13 6L21 6' stroke='%2321093A'/%3E%3Cpath d='M16 1L21 6L16 11' stroke='%2321093A'/%3E%3C/svg%3E%0A");
+  background-position: center center;
+  background-repeat: no-repeat;
+  cursor: pointer;
+  width: 52px;
+  height: 36px;
+  border-radius: 100px;
+  background-color: #AA93FF;
 }
 </style>
