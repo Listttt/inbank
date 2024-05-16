c67d10b2da3fdd54a4a8be21b88a1f62428d0d16
Dmitri Tanasiitsuk <dmitri.tanasiitsuk@bigwintek.com>
2024-05-16 22:48:11 +0300

    Styled calculator widget.

--- src/widgets/calculator/ui/IbCalculatorWidget.vue
+++ src/widgets/calculator/ui/IbCalculatorWidget.vue
@@ -90,52 +90,66 @@ onMounted(() => {
     </div>
 
     <div class="ib-calculator-widget_calculator">
-      <div class="ib-calculator-widget_calculator_amount">
-        <div class="ib-calculator-widget_calculator_amount_slider">
-          <ib-slider v-if="calcData.min" :min="calcData.min" :max="calcData.max" v-model.number="amount"/>
-          <div class="ib-calculator-widget_calculator_amount_slider_values">
+      <div class="ib-calculator-widget-inputs">
+
+        <div class="ib-calculator-widget_calculator_amount">
+          <div class="ib-calculator-widget_calculator_amount_slider">
+            <ib-slider v-if="calcData.min" :min="calcData.min" :max="calcData.max" v-model.number="amount"/>
+            <div class="ib-calculator-widget_calculator_amount_slider_values">
             <span>
               {{formatWithCurrency(calcData.min)}}
             </span>
-            <span>
+              <span>
               {{formatWithCurrency(calcData.max)}}
             </span>
+            </div>
           </div>
-        </div>
-        <div class="ib-calculator-widget_calculator_amount_input">
+          <div class="ib-calculator-widget_calculator_amount_input">
 
-<!--          TODO: add possibility to pass data for validation translations-->
-          <ib-input
-              ref="amountInput"
-              label="amount"
-              :validation="[
+            <!--          TODO: add possibility to pass data for validation translations-->
+            <ib-input
+                ref="amountInput"
+                label="amount"
+                :validation="[
                   (val) => (+val >= +`${calcData.min}`&& +val <= +`${calcData.max}`) || 'widget.calculator.input.amount.validation.error']"
-              v-model.number="amount"
-              type="number"
-          />
-        </div>
-      </div>
-
-      <div class="ib-calculator-widget_calculator_range">
-        <div class="ib-calculator-widget_calculator_range_slider">
-          <ib-slider v-if="periodStep" step="6" :min="calcData.periods[0]" :max="calcData.periods[calcData.periods.length -1]" v-model.number="period"/>
-          <div class="ib-calculator-widget_calculator_range_slider_values">
-            <ib-translated-text v-if="periodOptions.length"t-key="widget.calculator.range.min" :data="{value: periodOptions[0].value}"/>
-            <ib-translated-text v-if="periodOptions.length" t-key="widget.calculator.range.max" :data="{value: periodOptions[periodOptions.length - 1].value}"/>
+                v-model.number="amount"
+                type="number"
+            />
           </div>
         </div>
-        <div class="ib-calculator-widget_calculator_range_select">
-          <ib-select v-model.number="period" :options="periodOptions"/>
+
+        <div class="ib-calculator-widget_calculator_range">
+          <div class="ib-calculator-widget_calculator_range_slider">
+            <ib-slider v-if="periodStep" step="6" :min="calcData.periods[0]" :max="calcData.periods[calcData.periods.length -1]" v-model.number="period"/>
+            <div class="ib-calculator-widget_calculator_range_slider_values">
+              <ib-translated-text v-if="periodOptions.length"t-key="widget.calculator.range.min" :data="{value: periodOptions[0].value}"/>
+              <ib-translated-text v-if="periodOptions.length" t-key="widget.calculator.range.max" :data="{value: periodOptions[periodOptions.length - 1].value}"/>
+            </div>
+          </div>
+          <div class="ib-calculator-widget_calculator_range_select">
+            <ib-select v-model.number="period" :options="periodOptions"/>
+          </div>
         </div>
       </div>
 
-      <div class="ib-calculator-widget_calculator_result">
+      <div class="ib-calculator-widget-output">
+        <div class="ib-calculator-widget-output_result">
+          <div class="inter-400">
+            <ib-translated-text t-key="widget.calculator.output.payment"/>
+          </div>
 
-        {{payment}}
+          <div class="bitter-400">
+            {{payment}}
+          </div>
+        </div>
 
         <ib-button :disabled="amountInput?.invalid" @click="apply">
           <ib-translated-text class="ib-translated-text" t-key="widget.calculator.button.apply"/>
         </ib-button>
+
+        <div class="text-left inter-400 text-12-line-16">
+          <ib-translated-text t-key="widget.calculator.output.warning"/>
+        </div>
       </div>
     </div>
 
@@ -177,6 +191,12 @@ onMounted(() => {
   align-items: center;
   padding: 80px 40px !important;
   border-radius: 30px;
+
+  @media (max-width: 770px) {
+    flex-direction: column;
+    padding: 40px 16px !important;
+    gap: 40px;
+  }
 }
 
 .ib-calculator-widget_text {
@@ -186,6 +206,17 @@ onMounted(() => {
   align-items: center;
   width: 50%;
   padding: 72px 40px;
+
+  @media (max-width: 770px) {
+    padding: 0;
+    width: 100%;
+    display: block;
+
+
+    >.inter-400, >.bitter-400  {
+      text-align: start;
+    }
+  }
 }
 
 .ib-calculator-widget_calculator {
@@ -194,18 +225,41 @@ onMounted(() => {
   justify-content: center;
   align-items: center;
   width: 50%;
+  gap: 40px;
+  border-left: 1px solid rgba(33,9,58,.2);
+  padding-left: 40px;
+
+  @media (max-width: 770px) {
+    padding: 0;
+    border: none;
+    width: 100%
+  }
+}
+
+.ib-calculator-widget-inputs {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
 }
 
 .ib-calculator-widget_calculator_amount {
   display: flex;
   width: 100%;
   justify-content: space-between;
-  align-items: baseline;
   gap: 24px;
+
+  @media (max-width: 770px) {
+    flex-direction: column;
+  }
 }
 
 .ib-calculator-widget_calculator_amount_slider {
   width: inherit;
+
+  @media (max-width: 770px) {
+    order: 2
+  }
 }
 
 .ib-calculator-widget_calculator_amount_slider_values {
@@ -217,18 +271,31 @@ onMounted(() => {
 .ib-calculator-widget_calculator_amount_input {
   min-width: 160px;
   width: 160px;
+
+  @media (max-width: 770px) {
+    order: 1;
+    min-width: unset;
+    width: unset;
+  }
 }
 
 .ib-calculator-widget_calculator_range {
   display: flex;
   width: 100%;
   justify-content: space-between;
-  align-items: baseline;
   gap: 24px;
+
+  @media (max-width: 770px) {
+    flex-direction: column;
+  }
 }
 
 .ib-calculator-widget_calculator_range_slider {
   width: inherit;
+
+  @media (max-width: 770px) {
+    order: 2;
+  }
 }
 
 .ib-calculator-widget_calculator_range_slider_values {
@@ -239,6 +306,25 @@ onMounted(() => {
 
 .ib-calculator-widget_calculator_range_select {
   min-width: 160px;
+
+  @media (max-width: 770px) {
+    order: 1;
+  }
+}
+
+.ib-calculator-widget-output {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px
+}
+
+.ib-calculator-widget-output_result {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .ib-dialog_topic {
